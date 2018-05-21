7100890006171d507837d3a8b61a6503177569ea
Miguel Ángel Muñoz Viejo <miguelangelmunozviejo@MacBook-Pro-de-Miguel.local>
2018-05-21 11:26:59 +0200

    Creada la función de inicio obteniendo el canvas y la función principal del juego. También iniciado objeto Juego.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
-var vida = 100;
+var canvas, context, juego;
+var FPS = 50;
+
+function init() {
+    canvas = document.getElementById( 'juego' );
+    context = canvas.getContext( '2d' );
+    juego = new Juego(canvas, context);
+
+    setInterval(main, 1000/FPS);    
+}
+
+function main() {
+    // console.log( 'Frame' );
+    
+    juego.borraCanvas();
+}
+
+function Juego(canvas, context) {
+    this.canvas = canvas;
+    this.context = context;
+    this.escenario = {
+        tablero: [
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+        ],
+        anchoCelda: canvas.width / 12,
+        altoCelda: canvas.height / 9
+    };
+
+    // Métodos
+    this.borraCanvas = function() {
+        this.canvas.width=750;
+        this.canvas.height=500;
+    }
+}
